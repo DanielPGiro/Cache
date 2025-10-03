9bd512d4e6459f13af561770f95fc4855e7c0e63
DanielPGiro <danielgiro99@gmail.com>
2025-10-03 14:38:28 -0400

    Create state_machine.vhd

---
+++ VHDL/state_machine.vhd
@@ -0,0 +1,11 @@
+--
+-- Entity: negative edge triggered state machine
+-- Architecture : structural
+-- Author: Daniel Giro, Ian Lane
+--
+
+entity state_machine is
+  port( 
+
+  );
+end state_machine;
