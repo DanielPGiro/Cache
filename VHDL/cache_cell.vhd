fadd544e68dc354da83c2194118eb65bb11c08a8
ianlane1 <laneian@hotmail.com>
2025-10-05 22:34:34 -0400

    Changed rd_rw signals to input enable and output enable that can be controlled by state machine

--- VHDL/cache_cell.vhd
+++ VHDL/cache_cell.vhd
@@ -6,7 +6,8 @@ entity cache_cell is
   port (
     data_in : in std_logic;
     clk : in std_logic;
-    rd_wr : in std_logic;
+    IE : in std_logic; -- Input enable
+    OE : in std_logic; -- Output enable
     data_out : in std_logic
   );
 end cache_cell;
@@ -51,15 +52,15 @@ architecture structural of cache_cell is
   for dff_1: dff use entity work.dff(structural);
   for tx_1: tx use entity work.tx(structural);
 
-  signal rd_wr_inv, Dlatch_q, Dlatch_q_bar, dff_q, dff_q_bar: std_logic;
+  signal OE_inv, Dlatch_q, Dlatch_q_bar, dff_q, dff_q_bar: std_logic;
 
   begin 
 
-    inverter_1: inverter port map (rd_wr, rd_wr_inv);
-    Dlatch_1: Dlatch port map (data_in, rd_wr_inv, Dlatch_q, Dlatch_q_bar); -- We want to latch data on a write, which is active low, so we take the inverse.
+    inverter_1: inverter port map (OE, OE_inv);
+    Dlatch_1: Dlatch port map (data_in, IE, Dlatch_q, Dlatch_q_bar); -- We want to latch data on a write, which is active low, so we take the inverse.
     dff_1: dff port map(Dlatch_q, clk, dff_q, dff_q_bar); -- for the edge triggered dff, the output changes every clock cycle based on the input, therefore the input into the dff is the output of the dlatch, since the dlatch will maintain the same q value as long as wr is not enabled.
 
-    tx_1: tx port map(rd_wr, rd_wr_inv, dff_q, data_out); -- tx and Dlatch can not be enabled at the same time. 
+    tx_1: tx port map(OE, OE_inv, dff_q, data_out); -- tx and Dlatch can not be enabled at the same time. 
     -- Sel will go to pmos gate, selnot will go to nmos gate
 
 end structural;
--- VHDL/reg.vhd
+++ VHDL/reg.vhd
@@ -15,7 +15,8 @@ entity reg is
    bit_6_in : in std_logic; 
    bit_7_in : in std_logic; 
    clk : in std_logic;
-   rd_wr : in std_logic;
+   IE : in std_logic;
+   OE : in std_logic;
    bit_0_out : out std_logic;
    bit_1_out : out std_logic;
    bit_2_out : out std_logic;
@@ -32,7 +33,8 @@ architecture structural of reg is
     port (
       data_in : in std_logic;
       clk : in std_logic;
-      rd_wr : in std_logic;
+      IE : in std_logic; -- Input enable, same for every cell
+      OE : in std_logic; -- Output enable, same for every cell
       data_out : out std_logic
     );
   end component;
@@ -40,14 +42,14 @@ architecture structural of reg is
   for cell_0, cell_1, cell_2, cell_3, cell_4, cell_5, cell_6, cell_7: cache_cell use entity work.cache_cell(structural);
 
   begin
-    cell_0: cache_cell port map (bit_0_in, clk, rd_wr, bit_0_out);
-    cell_1: cache_cell port map (bit_1_in, clk, rd_wr, bit_1_out);
-    cell_2: cache_cell port map (bit_2_in, clk, rd_wr, bit_2_out);
-    cell_3: cache_cell port map (bit_3_in, clk, rd_wr, bit_3_out);
-    cell_4: cache_cell port map (bit_4_in, clk, rd_wr, bit_4_out);
-    cell_5: cache_cell port map (bit_5_in, clk, rd_wr, bit_5_out);
-    cell_6: cache_cell port map (bit_6_in, clk, rd_wr, bit_6_out);
-    cell_7: cache_cell port map (bit_7_in, clk, rd_wr, bit_7_out);
+    cell_0: cache_cell port map (bit_0_in, clk, IE, OE, bit_0_out);
+    cell_1: cache_cell port map (bit_1_in, clk, IE, OE, bit_1_out);
+    cell_2: cache_cell port map (bit_2_in, clk, IE, OE, bit_2_out);
+    cell_3: cache_cell port map (bit_3_in, clk, IE, OE, bit_3_out);
+    cell_4: cache_cell port map (bit_4_in, clk, IE, OE, bit_4_out);
+    cell_5: cache_cell port map (bit_5_in, clk, IE, OE, bit_5_out);
+    cell_6: cache_cell port map (bit_6_in, clk, IE, OE, bit_6_out);
+    cell_7: cache_cell port map (bit_7_in, clk, IE, OE, bit_7_out);
 
 end structural;
 
