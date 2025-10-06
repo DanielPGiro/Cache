4c87b164d4f8c45b3849f84af076ef4578b9a9a1
ianlane1 <ianl1@umbc.edu>
2025-10-06 12:10:54 -0400

    Update and rename VHDL/4byte_reg.vhd to block.vhd

---
+++ block.vhd
@@ -0,0 +1,12 @@
+--
+-- Entity: negative edge triggered block (4-byte register)
+-- Architecture : structural
+-- Author: Daniel Giro, Ian Lane
+-- There will be four of these in the cache, totaling 16 bytes
+--
+
+entity 4byte_reg is
+  port(
+
+  );
+end 4byte_reg;
