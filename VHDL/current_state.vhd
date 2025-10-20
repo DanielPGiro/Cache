e84f0bd56263ed7ba9a2053507e3d35c134765a4
DanielPGiro <danielgiro99@gmail.com>
2025-10-20 12:32:32 -0400

    Add initial structure for current_state VHDL file

---
+++ VHDL/current_state.vhd
@@ -0,0 +1,32 @@
+--
+-- Entity: negative edge triggered cache memory (cache_mem)
+-- Architecture: structural
+-- Author: Daniel Giro, Ian Lane
+
+library IEEE;
+library STD;
+use IEEE.std_logic_1164.all;
+
+
+entity  is
+  port (
+    
+  );
+
+end cache mem;
+
+architecture structural of current_state is
+  component 
+    port (
+      
+    );
+  end component;
+
+for cs: use entity ;
+
+signal ;
+
+begin
+
+
+end structural;
