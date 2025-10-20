bb9a60cec0a8dccf586aee9c123b6f3347e357b2
DanielPGiro <danielgiro99@gmail.com>
2025-10-20 11:30:47 -0400

    Refactor counter testbench structure and processes
    Refactor counter testbench for improved clarity and organization.

--- TestBenches/counter_testbench.vhd
+++ TestBenches/counter_testbench.vhd
@@ -1,9 +1,6 @@
-
 -- Input
 -- Line 1: clk
 -- Line 2: rst
-
-
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -11,64 +8,60 @@ use IEEE.std_logic_textio.all;
 use STD.textio.all;
   
 entity counter_test is
-  
 end counter_test;
   
 architecture test of counter_test is
 
-component counter
-  port (
-    clk     : in  std_logic;
-    rst		: in  std_logic;
-    Q       : inout std_logic_vector(3 downto 0));
-end component;
-
-for counter_0 : counter use entity work.counter(structural);
-
-signal Qout : std_logic_vector(3 downto 0);
-signal reset, clock : std_logic;
+  component counter
+    port (
+      clk     : in  std_logic;
+      rst		: in  std_logic;
+      Q       : inout std_logic_vector(3 downto 0));
+  end component;
+  
+  for counter_0 : counter use entity work.counter(structural);
+  
+  signal Qout : std_logic_vector(3 downto 0);
+  signal reset, clock : std_logic;
+  
+  procedure print_output is
+    variable outline: line;
+  
+  begin
+    write(outline, string'("Clk = "));
+    write(outline, clk);
+    write(outline, string'("Reset = "));
+    write(outline, reset);
+    write(outline, string'("Q = "));
+    write(outline, Qout);
+  end print_output;
 
 begin
-  
-counter_0 : counter port map (clock, reset, Qout);
-  
-io_process: process
-  file infile  : text is in  "counter_test_in.txt";
-  file outfile : text is out "counter_test_out.txt";
-  variable op1 : std_logic;
-  variable buf : line;
-    
-begin
-
-while not (endfile(infile)) loop
-    readline(infile, buf);
-    read(buf, op1);
-    clock<=op1;
-
-    readline(infile, buf);
-    read(buf, op1);
-    reset<=op1;
-
+  counter_0 : counter port map (clock, reset, Qout);
+  clk : process
+  begin
+    clock <= '0', '1' after 5 ns;
     wait for 10 ns;
-
-    write(buf, string'("--------------"));
-    writeline(outfile, buf);
-
-    write(buf, string'("Clk = "));
-    write(buf, clock);
-    writeline(outfile, buf);
-
-    write(buf, string'("Rst = "));
-    write(buf, reset);
-    writeline(outfile, buf);
-
-    write(buf, string'("Q = "));
-    write(buf, Qout);
-    writeline(outfile, buf);
-
-end loop; 
-end process io_process;
-
+  end process clk;
+    
+  sim_process: process
+  begin
+    reset <= '1';
+    wait for 1 ns;
+    print_output;
+    wait until falling_edge(clock);
+    reset <= '0';
+    wait until falling_edge(clock);
+    print_output;
+    wait until falling_edge(clock);
+    print_output;
+    wait until falling_edge(clock);
+    print_output;
+    wait until falling_edge(clock);
+    print_output;
+    wait until falling_edge(clock);
+    print_output;
+  end process sim_process;
 end test;
 
 
