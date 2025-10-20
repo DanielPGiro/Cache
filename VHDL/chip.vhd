5d5e08158e54f370c75b04c8a114e67fdab7faef
DanielPGiro <danielgiro99@gmail.com>
2025-10-20 12:24:40 -0400

    Add state_machine and cache_mem components

--- VHDL/chip.vhd
+++ VHDL/chip.vhd
@@ -26,8 +26,47 @@ entity chip is
 end chip;
 
 architecture structural of chip is
-    component 
-    begin
-      something
+    component state_machine
+        port (
+            cpu_rd_wrn : in  std_logic;    
+            start      : in  std_logic;
+            clk        : in  std_logic;
+            reset      : in  std_logic;
+            Vdd	       : in  std_logic;
+            Gnd        : in  std_logic;
+            valid      : in std_logic;
+            busy       : in std_logic;
+            IE         : out std_logic;
+            OE         : out std_logic_vector(5 downto 0)
+        );
+    end component;
+        
+    component cache_mem
+        port (
+            byte_in : in std_logic_vector(7 downto 0); -- Mem for write
+            CA : in std_logic_vector(3 downto 0); -- for block/byte decode
+            tag_set : in std_logic_vector(1 downto 0); -- Data for new tag
+            tag_enable : in std_logic; -- High for tag change
+            valid_set : in std_logic; 
+            valid_enable : in std_logic;
+            VDD : in std_logic;
+            clk : in std_logic;
+            IE : in std_logic; -- Controlled by state machine, mapped to every reg
+            OE : in std_logic;
+            reset : in std_logic;
+            byte_out : out std_logic_vector(7 downto 0); -- For read
+            valid_out : out std_logic;
+            tag_out : out std_logic_vector(1 downto 0)
+        );
+    end component;
+
+    for sm: use entity work.state_machine(structural);
+    for cmem: use entity work.cache_mem(structural);
+
+   -- signal somethingtoadd
+    
+    begin 
+        sm: port map ();
+        cmem: port map();
 
 end structural;
