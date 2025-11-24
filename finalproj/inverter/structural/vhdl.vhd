046f58cef2abec934f4b45f51b7adabc2fac2f17
ianlane1 <laneian@hotmail.com>
2025-11-24 17:10:29 -0500

    added zip file

--- finalproj/inverter/structural/vhdl.vhd
+++
@@ -1,8 +0,0 @@
--- Created by @(#)$CDS: vhdlin version IC23.1-64b 06/21/2023 09:20 (cpgbld16) $
--- on Mon Nov  3 12:21:32 2025
-
-
-architecture structural of inverter is
-begin
-	output <= not (input);
-end structural;
